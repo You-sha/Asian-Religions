f2be7812970485de6d5a8b4297a3879468fbf0b1
Yousha <123200960+You-sha@users.noreply.github.com>
2023-03-27 04:15:06 +0530

    Started NLP analysis

--- Cleaning.py
+++ Cleaning.py
@@ -40,10 +40,10 @@ stopwords = set(stopwords.words('english'))
 filtered_list = [word for word in word_string if word.casefold() not in stopwords]
 
 #stemming
-from nltk.stem import PorterStemmer
-stremmer = PorterStemmer()
+# from nltk.stem import PorterStemmer
+# stremmer = PorterStemmer()
 
-stemmed = [stremmer.stem(word) for word in filtered_list]
+# stemmed = [stremmer.stem(word) for word in filtered_list]
 
 #pos tagging
 tagged = pd.DataFrame(nltk.pos_tag(word_string), columns=['words','tag'])
@@ -53,6 +53,44 @@ tagged['punc'] = tagged.tag.apply(lambda x: 1 if x in punc else 0)
 tagged = tagged.drop(tagged[tagged.punc == 1].index).drop('punc',axis=1)
 
 tagged.tag.value_counts()
+
+#lemmatizing
+from nltk.stem import WordNetLemmatizer
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+lemmatizer = WordNetLemmatizer()
+
+tagged['lemmatized'] = [lemmatizer.lemmatize(word) for word in tagged['words']]
+
+tagged.loc[tagged['lemmatized'] != tagged['words']].shape[0]
+
+#chunking
+grammar = "NP: {<DT>?<JJ>*<NN>}"
+chunk_parser = nltk.RegexpParser(grammar)
+
+tree = chunk_parser.parse(list(zip(tagged['lemmatized'],tagged['tag'])))
+tree.draw()
+
+#chinking
+grammar = """
+    Chunk: {<.>+.}
+           }<JJ>{"""
+chink_parser = nltk.RegexpParser(grammar)
+
+tree = chink_parser.parse(list(zip(tagged['lemmatized'],tagged['tag'])))
+tree.draw()
+
+#using name entity recognition
+nltk.download("maxent_ne_chunker")
+nltk.download("words")
+
+tree = nltk.ne_chunk(list(zip(tagged['lemmatized'],tagged['tag'])))
+tree.draw()
+
+
+
+
+
 
 
 
